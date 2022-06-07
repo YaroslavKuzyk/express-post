ef28c2b56643d92b573158f86984131e7260c7ea
Yaroslav Kuzyk <kuzyk.yaril@gmail.com>
2022-06-07 20:27:07 +0300

    fist commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import router from "./router.js";
+import fileUpload from "express-fileupload";
+
+const PORT = 5000;
+const DB_URL =
+  "mongodb+srv://admin:admin@cluster0.inoix.mongodb.net/?retryWrites=true&w=majority";
+const app = express();
+
+app.use(express.json());
+app.use(express.static("static"));
+app.use(fileUpload({}));
+app.use("/api", router);
+
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL);
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+}
+startApp();
